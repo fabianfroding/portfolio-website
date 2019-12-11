cf6a801559d3b796b1b654283e7cca50848edaa0
fabianfroding <fabian.froding@gmail.com>
2019-12-11 22:14:31 +0100

    Updated project view

--- PortfolioWebsite/PortfolioWebsite/Controllers/ProjectsController.cs
+++ PortfolioWebsite/PortfolioWebsite/Controllers/ProjectsController.cs
@@ -10,6 +10,15 @@ namespace PortfolioWebsite.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.ProjectTitle = "Vault of Darkness";
+            ViewBag.Description = "A mod for Warcraft III";
+            ViewBag.Type = "Game Mod";
+            ViewBag.Pictures = new String[]
+            {
+                "Pic1",
+                "Pic2",
+                "Pic3"
+            };
             return View();
         }
     }
