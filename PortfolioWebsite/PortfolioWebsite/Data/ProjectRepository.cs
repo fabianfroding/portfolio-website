a867e57c7fae466c2ab36b3ae5b8b207b5dfce22
fabianfroding <fabian.froding@gmail.com>
2019-12-12 22:13:20 +0100

    Added project repository

---
+++ PortfolioWebsite/PortfolioWebsite/Data/ProjectRepository.cs
@@ -0,0 +1,63 @@
+﻿using PortfolioWebsite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PortfolioWebsite.Data
+{
+    public class ProjectRepository
+    {
+        private static Project[] _Projects = new Project[]
+        {
+            new Project()
+            {
+                ProjectTitle = "Vault of Darkness",
+                Description = "A mod for Warcraft III",
+                Images = new String[]
+                {
+                    "~/Images/screenshots-vod/1.png",
+                    "~/Images/screenshots-vod/2.png",
+                    "~/Images/screenshots-vod/3.jpg"
+                }
+            },
+            new Project()
+            {
+                ProjectTitle = "Celestial Walkway",
+                Description = "A mod for Warcraft III",
+                Images = new String[]
+                {
+                    "~/Images/screenshots-vod/1.png",
+                    "~/Images/screenshots-vod/2.png",
+                    "~/Images/screenshots-vod/3.jpg"
+                }
+            },
+            new Project()
+            {
+                ProjectTitle = "RPG",
+                Description = "A mod for Warcraft III",
+                Images = new String[]
+                {
+                    "~/Images/screenshots-vod/1.png",
+                    "~/Images/screenshots-vod/2.png",
+                    "~/Images/screenshots-vod/3.jpg"
+                }
+            }
+        };
+        public Project GetProject(int Id)
+        {
+            foreach (var project in _Projects)
+            {
+                if (project.Id == Id)
+                {
+                    return project;
+                }
+            }
+            return null;
+        }
+        public Project[] GetProjects()
+        {
+            return _Projects;
+        }
+    }
+}
