c7c751eb73a28440aedb577a1cf30dc9cac8b02c
fabianfroding <fabian.froding@gmail.com>
2019-12-28 17:31:54 +0100

    Attempted to seed database using code first migration. Create migraton PopulateProjects

---
+++ PortfolioWebsite/PortfolioWebsite/Migrations/201912281147543_PopulateProjects.cs
@@ -0,0 +1,20 @@
+﻿namespace PortfolioWebsite.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateProjects : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Projects (Title, Description) VALUES ('Role Detector', 'A software project')");
+            Sql("INSERT INTO Projects (Title, Description) VALUES ('Dog watcher', 'Hardware to observe dog behaviour')");
+
+
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
