b49917459517479e6614ccb2a8f760136a39e815
fabianfroding <fabian.froding@gmail.com>
2019-12-30 12:41:25 +0100

    Started setting up project processor for sql data access

---
+++ PortfolioWebsite/PortfolioWebsite/BusinessLogic/ProjectProcessor.cs
@@ -0,0 +1,32 @@
+﻿using PortfolioWebsite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PortfolioWebsite.BusinessLogic
+{
+    public static class ProjectProcessor
+    {
+        public static string ImagesToString(Project project)
+        {
+            if (project.Images.Length == 0)
+            {
+                return null;
+            }
+            string s = "";
+            for (int i = 0; i < project.Images.Length; i++)
+            {
+                if (i < project.Images.Length - 1)
+                {
+                    s += project.Images[i] + ",";
+                }
+                else
+                {
+                    s += project.Images[i];
+                }
+            }
+            return s;
+        }
+    }
+}
