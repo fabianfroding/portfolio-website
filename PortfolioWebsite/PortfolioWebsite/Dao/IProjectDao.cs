be7af01b10dacbc6c86c0078c393bbbd46319651
fabianfroding <fabian.froding@gmail.com>
2020-01-07 11:22:08 +0100

    Started setting up Dao pattern for separating db access from controller.

---
+++ PortfolioWebsite/PortfolioWebsite/Dao/IProjectDao.cs
@@ -0,0 +1,13 @@
+﻿using PortfolioWebsite.Models;
+using System.Collections.Generic;
+
+namespace PortfolioWebsite.Dao
+{
+    public interface IProjectDao
+    {
+        List<Project> findAll();
+        Project findById(int id);
+        void save(Project project);
+        void delete(Project project);
+    }
+}
---
+++ PortfolioWebsite/PortfolioWebsite/Dao/ProjectDao.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PortfolioWebsite.Models;
+
+namespace PortfolioWebsite.Dao
+{
+    public class ProjectDao : IProjectDao
+    {
+        public void delete(Project project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Project> findAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Project findById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void save(Project project)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
