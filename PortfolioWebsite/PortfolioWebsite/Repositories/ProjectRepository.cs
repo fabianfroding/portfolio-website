bb9825df56385115265946a02b66380cb658486e
fabianfroding <fabian.froding@gmail.com>
2020-01-14 11:53:55 +0100

    Merged the static project array from ProjectRepositotyLocal into ProjectRepository

--- PortfolioWebsite/PortfolioWebsite/Repositories/ProjectRepository.cs
+++ PortfolioWebsite/PortfolioWebsite/Repositories/ProjectRepository.cs
@@ -8,26 +8,73 @@ namespace PortfolioWebsite.Repositories
 {
     public class ProjectRepository : IProjectRepository, IDisposable
     {
+        // Move this stuff to db initializer when it is implemented.
+        private static Project[] _projects = new Project[]
+        {
+            new Project()
+            {
+                Id = 1,
+                Title = "Vault of Darkness",
+                Description = "A mod for Warcraft III",
+                Images = new List<string>()
+                {
+                    "~/Images/temp/vod-1.png",
+                    "~/Images/temp/vod-2.png",
+                    "~/Images/temp/vod-3.jpg",
+                    "~/Images/temp/vod-4.png"
+                }
+            },
+
+            new Project()
+            {
+                Id = 2,
+                Title = "My siberia notebook",
+                Description = "An app to register events in siberia",
+                Images = new List<string>()
+                {
+                    "~/Images/temp/husky-1.jpg",
+                    "~/Images/temp/husky-2.jpg",
+                    "~/Images/temp/husky-3.jpg"
+                }
+            },
+
+            new Project()
+            {
+                Id = 3,
+                Title = "Wish",
+                Description = "A game in unity",
+                Images = new List<string>()
+                {
+                    "~/Images/temp/wish-1.jpg",
+                    "~/Images/temp/wish-2.jpg"
+                }
+            }
+        };
+
         private ProjectContext _projectContext;
-        private ProjectRepositoryLocal _projectRepositoryLocal; // Temp, remove when replaced with context
 
         public ProjectRepository()
         {
             _projectContext = new ProjectContext();
-            _projectRepositoryLocal = new ProjectRepositoryLocal();
         }
 
         public Project GetById(int id)
         {
-            System.Diagnostics.Debug.WriteLine("Repository GetById: " + _projectRepositoryLocal.GetProject(id).Images.Count.ToString());
-            return _projectRepositoryLocal.GetProject(id);
-            //var project = _projectContext.Projects.SingleOrDefault(c => c.Id == id);
+            foreach (var project in _projects)
+            {
+                if (project.Id == id)
+                {
+                    return project;
+                }
+            }
+            return null;
+            //return _projectContext.Projects.SingleOrDefault(c => c.Id == id);
         }
 
         public List<Project> GetAll()
         {
-            return _projectRepositoryLocal.GetProjects().ToList();
-            //var projects = _projectContext.Projects.ToArray<Project>();
+            return _projects.ToList();
+            //return _projectContext.Projects.ToArray<Project>();
         }
 
         public void Add(Project project)
