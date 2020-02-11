0afe96e457f001795707de6bfe6ac04354eedec9
fabianfroding <fabian.froding@gmail.com>
2020-02-11 21:34:42 +0100

    Changed the stickynav.js script (which wasnt working in the first place) to now make navbar smaller when scrolling down.

--- PortfolioWebsite/PortfolioWebsite/Scripts/stickynav.js
+++ PortfolioWebsite/PortfolioWebsite/Scripts/stickynav.js
@@ -1,10 +1,34 @@
-﻿var mh = $(".main-header");
+﻿window.onscroll = function () { scrollFunction() };
 
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 250) {
-        mh.addClass("nav-scrolled");
+function scrollFunction() {
+    if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+
+        var elements = document.getElementsByClassName("main-header");
+        var nav = document.getElementsByClassName("nav");
+        var siteLogoImage = document.getElementsByClassName("site-logo-image");
+        elements[0].style.height = "70px";
+        nav[0].style.paddingRight = "3em";
+        siteLogoImage[0].style.width = "70px";
+        siteLogoImage[0].style.height = "30px";
+
+        var navItems = document.getElementsByClassName("nav-item");
+        for (var i = 0; i < navItems.length; i++) {
+            navItems[i].style.fontSize = "0.875em";
+        }
+
+    } else {
+
+        var elements = document.getElementsByClassName("main-header");
+        var nav = document.getElementsByClassName("nav");
+        var siteLogoImage = document.getElementsByClassName("site-logo-image");
+        elements[0].style.height = "80px";
+        nav[0].style.paddingRight = "1em";
+        siteLogoImage[0].style.width = "85px";
+        siteLogoImage[0].style.height = "41px";
+
+        var navItems = document.getElementsByClassName("nav-item");
+        for (var i = 0; i < navItems.length; i++) {
+            navItems[i].style.fontSize = "1.125em";
+        }
     }
-    else {
-        mh.removeClass("nav-scrolled");
-    }
-});
+}
